8d8bedea3ef92b7ca689775ca70ea42e497d5221
angelbulseco22 <marbulseco@teligentsystems.ph>
2015-02-09 11:09:08 +0800

    Changed to static method

--- src/main/java/com/pnb/systematics/configuration/DateUtil.java
+++ src/main/java/com/pnb/systematics/configuration/DateUtil.java
@@ -4,21 +4,21 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DateUtil {
+public final class DateUtil {
 
-	public String getCurrentHour(){
+	public static String getCurrentHour(){
 		DateFormat dateFormat = new SimpleDateFormat("HH");
 		Date date = new Date();
 		return dateFormat.format(date);
 	}
 	
-	public String getCurrentSecond(){
+	public static String getCurrentSecond(){
 		DateFormat dateFormat = new SimpleDateFormat("ss");
 		Date date = new Date();
 		return dateFormat.format(date);
 	}
 	
-	public String getCurrentMinutes(){
+	public static String getCurrentMinutes(){
 		DateFormat dateFormat = new SimpleDateFormat("mm");
 		Date date = new Date();
 		return dateFormat.format(date);
